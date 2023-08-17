909345e8ce99b42d0b4a14ae498e78c2ba3c352b
khadija swaleh <khadija12345@hotmail.co.uk>
2023-08-17 19:49:04 +0100

    changed type to style in controller

--- app/models/hat.rb
+++ app/models/hat.rb
@@ -1,6 +1,6 @@
 class Hat < ApplicationRecord
   belongs_to :user
 
-  validates :color,  presence: true
+  validates :color, :style, presence: true
   validates :price, presence: true,  format: { with: /\A\d+(?:\.\d{0,2})?\z/ }, numericality: { greater_than_or_equal_to: 1 }
 end
