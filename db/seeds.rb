9f44ee3811482f6d1f189bf24a216835709576d6
Priscila Finkler <prifinkler@gmail.com>
2023-08-17 22:57:58 +0100

    added cards component and updated hats index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Hat.create!(style: 'Fedora', color: 'Black', price: 13, user_id: 1)
+Hat.create!(style: 'Baseball Cap', color: 'Blue', price: 17, user_id: 1)
+Hat.create(style: 'Sun Hat', color: 'Beige', price: 22, user_id: 1)
+Hat.create(style: 'Beanie', color: 'Gray', price: 54, user_id: 1)
+Hat.create(style: 'Cowboy Hat', color: 'Brown', price: 4, user_id: 1)
