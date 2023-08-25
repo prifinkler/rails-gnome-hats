87f115d2ef7faa90974e71672c2abf48fb004094
Priscila Finkler <prifinkler@gmail.com>
2023-08-25 23:57:09 +0100

    added reviews carousel first draft on hat show page

--- app/models/hat.rb
+++ app/models/hat.rb
@@ -1,6 +1,15 @@
 class Hat < ApplicationRecord
   belongs_to :user
   has_one_attached :photo
-  validates :color, :style, :description,  presence: true
+
+  validates :color, :style, :description, presence: true
   validates :price, presence: true, format: { with: /\A\d+(?:\.\d{0,2})?\z/ }, numericality: { greater_than_or_equal_to: 1 }
+
+  before_destroy :destroy_related_bookings
+
+  private
+
+  def destroy_related_bookings
+    user.bookings.where(hat_id: id).destroy_all
+  end
 end
